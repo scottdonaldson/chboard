43f197064564d28d2fa0b63aef4bda3d50a29091
Scott Donaldson <scott.p.donaldson@gmail.com>
2013-02-22 15:49:51 -0600

    feb package

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <p><a href="<?php echo bloginfo('template_url'); ?>/pdf/board-phone-list.pdf">Contact List</a></p>
 <p><a href="<?php echo bloginfo('template_url'); ?>/pdf/meetings-by-month.pdf">Calendar (PDF)</a></p>
+<p><a href="<?php echo bloginfo('template_url'); ?>/pdf/feb-board-package.pdf">January Board Package</a></p>
 <p><a href="<?php echo bloginfo('template_url'); ?>/pdf/january-package.pdf">January Board Package</a></p>
 <p><a href="<?php echo bloginfo('template_url'); ?>/pdf/2011-990.pdf">2011 Audit/990</a></p>
